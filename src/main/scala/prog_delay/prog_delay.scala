b2e4fa76714ce644e92f76ad07805e1bdd5e5253
Marko Kosunen <marko.kosunen@aalto.fi>
2019-01-06 14:15:01 -0800

    Initial commit

--- src/main/scala/prog_delay/prog_delay.scala
+++ src/main/scala/prog_delay/prog_delay.scala
@@ -1,32 +1,44 @@
-
-// Dsp-block prog_delay
-// Description here 
-// Inititally written by dsp-blocks initmodule.sh, 20190106
+// See LICENSE for license details.
+//
+//Start with a static tb and try to genererate a gnerator for it
 package prog_delay
-
-import chisel3.experimental._
 import chisel3._
+import chisel3.util._
+import chisel3.experimental._
 import dsptools._
 import dsptools.numbers._
-import breeze.math.Complex
 
-class prog_delay_io[T <:Data](proto: T,n: Int) 
-   extends Bundle {
-        val A       = Input(Vec(n,proto))
-        val B       = Output(Vec(n,proto))
-        override def cloneType = (new prog_delay_io(proto.cloneType,n)).asInstanceOf[this.type]
-   }
-
-class prog_delay[T <:Data] (proto: T,n: Int) extends Module {
-    val io = IO(new prog_delay_io( proto=proto, n=n))
-    val register=RegInit(VecInit(Seq.fill(n)(0.U.asTypeOf(proto.cloneType))))
-    register:=io.A
-    io.B:=register
+class prog_delay_io[ T <: Data]( proto: T, val maxdelay: Int=64) extends Bundle {
+   requireIsChiselType(proto.cloneType)
+   val iptr_A= Input(proto.cloneType)
+   val optr_Z= Output(proto.cloneType)
+   val select= Input(UInt(log2Ceil(maxdelay).W))
+   override def cloneType = (new prog_delay_io(proto.cloneType,maxdelay)).asInstanceOf[this.type]
 }
 
-//This gives you verilog
+
+//Class of programmable
+class prog_delay[ T <: Data ] (val proto: T, val maxdelay: Int=64)extends Module{
+    val io=IO(
+        new prog_delay_io(proto,maxdelay)
+       )
+    // Both work, but asTypeOf should provide less "problems"
+    //val zero=Ring[T].zero
+    val zero=0.U.asTypeOf(proto.cloneType)
+    val inreg=RegInit(zero)
+    inreg:=io.iptr_A
+    val regarray=Reg(Vec(maxdelay,proto.cloneType))
+    regarray.foldLeft(inreg) {(prev,next)=>next:=prev; next} //The last "next" is the return value that becomes the prev
+
+    val oreg=RegInit(zero)
+    oreg:=regarray(io.select)
+    io.optr_Z:=oreg
+}
+
+
 object prog_delay extends App {
-    chisel3.Driver.execute(args, () => new prog_delay(
-        proto=DspComplex(UInt(16.W),UInt(16.W)), n=8) 
-    )
+  val n=16
+  val proto=DspComplex(SInt(n.W), SInt(n.W))
+  chisel3.Driver.execute(args, () => new prog_delay( proto=proto.cloneType, maxdelay=64 ))
 }
+
