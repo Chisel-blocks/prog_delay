342f0debe1bd949b62eeab61cca4b3cc46081afc
Marko Kosunen <marko.kosunen@aalto.fi>
2019-02-04 17:15:01 -0800

    Convert structure to log2-weighted bypass

--- src/main/scala/prog_delay/prog_delay.scala
+++ src/main/scala/prog_delay/prog_delay.scala
@@ -8,6 +8,7 @@ import chisel3.experimental._
 import dsptools._
 import dsptools.numbers._
 
+
 class prog_delay_io[ T <: Data]( proto: T, val maxdelay: Int=64) extends Bundle {
    requireIsChiselType(proto.cloneType)
    val iptr_A= Input(proto.cloneType)
@@ -27,18 +28,27 @@ class prog_delay[ T <: Data ] (val proto: T, val maxdelay: Int=64)extends Module
     val zero=0.U.asTypeOf(proto.cloneType)
     val inreg=RegInit(zero)
     inreg:=io.iptr_A
-    
-    val regarray=Reg(Vec(maxdelay,proto.cloneType))
-    regarray.foldLeft(inreg) {(prev,next)=>next:=prev; next} //The last "next" is the return value that becomes the prev
+    val stage=Wire(Vec(log2Ceil(maxdelay)+1,proto.cloneType))
+    val stageo=Wire(Vec(log2Ceil(maxdelay),proto.cloneType))
+    stage(0):=inreg
+    val i=Seq.range(0,log2Ceil(maxdelay))
+    val pipes=i.map(i=>Module(new Pipe(proto.cloneType,latency=scala.math.pow(2,i).toInt)).io)
+
+    for ( index <- 0 to log2Ceil(maxdelay)-1 ){
+        pipes(index).enq.valid:=true.B
+        pipes(index).enq.bits:=stage(index)
+        stageo(index):=pipes(index).deq.bits
+        stage(index+1):=Mux(io.select(index),stageo(index),stage(index))
+    }
 
     val oreg=RegInit(zero)
-    oreg:=regarray(io.select)
+    oreg:=stage(log2Ceil(maxdelay))
     io.optr_Z:=oreg
 }
 
 
 object prog_delay extends App {
-  val n=16
+  val n=15
   val proto=DspComplex(SInt(n.W), SInt(n.W))
   chisel3.Driver.execute(args, () => new prog_delay( proto=proto.cloneType, maxdelay=64 ))
 }
