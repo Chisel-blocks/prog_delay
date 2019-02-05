9656cca05e59ea16fa5c3ad3f0d961cad364b229
Marko Kosunen <marko.kosunen@aalto.fi>
2019-02-04 20:49:31 -0800

    Fixup

--- src/main/scala/prog_delay/prog_delay.scala
+++ src/main/scala/prog_delay/prog_delay.scala
@@ -13,7 +13,7 @@ class prog_delay_io[ T <: Data]( proto: T, val maxdelay: Int=64) extends Bundle 
    requireIsChiselType(proto.cloneType)
    val iptr_A= Input(proto.cloneType)
    val optr_Z= Output(proto.cloneType)
-   val select= Input(UInt(log2Floor(maxdelay).W))
+   val select= Input(UInt(log2Ceil(maxdelay).W))
    override def cloneType = (new prog_delay_io(proto.cloneType,maxdelay)).asInstanceOf[this.type]
 }
 
@@ -28,13 +28,13 @@ class prog_delay[ T <: Data ] (val proto: T, val maxdelay: Int=64)extends Module
     val zero=0.U.asTypeOf(proto.cloneType)
     val inreg=RegInit(zero)
     inreg:=io.iptr_A
-    val stage=Wire(Vec(log2Floor(maxdelay)+1,proto.cloneType))
-    val stageo=Wire(Vec(log2Floor(maxdelay),proto.cloneType))
+    val stage=Wire(Vec(log2Ceil(maxdelay)+1,proto.cloneType))
+    val stageo=Wire(Vec(log2Ceil(maxdelay),proto.cloneType))
     stage(0):=inreg
-    val i=Seq.range(0,log2Floor(maxdelay))
+    val i=Seq.range(0,log2Ceil(maxdelay))
     val pipes=i.map(i=>Module(new Pipe(proto.cloneType,latency=scala.math.pow(2,i).toInt)).io)
 
-    for ( index <- 0 to log2Floor(maxdelay)-1 ){
+    for ( index <- 0 to log2Floor(maxdelay) ){
         pipes(index).enq.valid:=true.B
         pipes(index).enq.bits:=stage(index)
         stageo(index):=pipes(index).deq.bits
